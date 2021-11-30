b19d09f0198a5cbbdf680c32fd3c64db4ba8a118
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2021-11-30 08:44:37 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,30 +1,35 @@
 
 
 public class Course{
-   private Department dept;
-   private String title; // title of the course (e.g. Intro to programming);
-   private String code; // course code, e.g. SOFE, ELEE, MANE, etc.
-   private int number; // course number, e.g. 1200, 2710, 2800, etc.
-   private  Vector <Student>classList; // contains all students registered in this course
+	private Department dept;
+	private String title; // title of the course (e.g. Intro to programming);
+	private String code; // course code, e.g. SOFE, ELEE, MANE, etc.
+	private int number; // course number, e.g. 1200, 2710, 2800, etc.
+	private  Vector <Student>classList; // contains all students registered in this course
+	
+	public Course() {}
+ 	
+	public Course(String code, int number, Department dept, String title) {
+		this.dept = dept;
+		this.code = code;
+		this.number = number;
+		this.title = title;
+		this.classList = new Vector<Student>;
+	}
 
-   public Course(String code, int number, Department dept, String title) {
-	      // also initialize the classList;
-	 	   
-	   }
-
-   
-   public Course() {
-	// TODO Auto-generated constructor stub
-}
- 
-  public String toString() {
-     // return a string representation of the course with the course code,
-     // name, and number of people registered in the course in the following
-     // format:
-     // SOFE 2710 Object Oriented Programming and Design, Enrollment = 260
-
-     return null;
-  }
+   	public String getDept(){
+		return this.Dept.toString;
+	}
+	
+	public int getCode(){}
+	
+	public String toString() {
+     		// return a string representation of the course with the course code,
+     		// name, and number of people registered in the course in the following
+     		// format:
+     		// SOFE 2710 Object Oriented Programming and Design, Enrollment = 260
+		return (this.code + " " + this.number + " " + this.title + ", Enrollment = " + classList.length());
+  	}
 
 
 
