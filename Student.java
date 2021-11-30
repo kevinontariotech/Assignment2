9711594edbd62ba881a533d77585472c87f91ece
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2021-11-30 08:58:08 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,30 +1,34 @@
 public class Student extends Person{   
-  private String id;
-  private String name;
-  private Vector courses; // contains all courses the student is registered in
+  	private String id;
+  	private String name;
+  	private Vector<Course> courses; // contains all courses the student is registered in
 
 
 	public Student(String stdName, String stdId) { 
-	   
+		this.id = stdId;
+		this.name = stdName;
+		this.courses = new Vector<Course>;
 	}
 
 
-  	  public String getName() {  		 
-  		 	 
-  	  }
+  	public String getName() { 
+		return name;
+  	}
 
-  	  public String getId() {
-  		  
-  	  } 	  
+  	public String getId() {
+  		return id;  
+  	} 	  
   	  
-  	  
-  	
-
-    public String toString() {
-     // return a string representation of a student using the following format:
-     // 100234546 John McDonald
-     // Registered courses: ELEE 2110, ELEE 2790, SOFE 2710, SOFE 2800, SOFE 2850
-   
-	return null; 
-  }
+    	public String toString() {
+     		// return a string representation of a student using the following format:
+     		// 100234546 John McDonald
+     		// Registered courses: ELEE 2110, ELEE 2790, SOFE 2710, SOFE 2800, SOFE 2850
+   		String r = this.id + " " + this.name + "\nRegistered courses: ";
+		
+		foreach (Course c : courses){
+			r += c.toString;
+		}
+		
+		return r; 
+  	}
 }
