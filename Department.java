0d06e583524fa9ffedfa0f5ed3071c3316aaa473
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2021-11-30 08:48:39 -0500

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,31 +1,34 @@
- import java.util.HashSet;
+import java.util.HashSet;
 import java.util.Vector;
 
 public class Department {
-   private String name; // the name of school Dept of Computing and Info Science
-   private String id; // short name for courses SOFE, ELEE, STAT, etc
-   private Vector<Course> courseList; // all courses offered by the department
-   private Vector<Student> registerList; // all students taking courses in the department.
+   	private String name; // the name of school Dept of Computing and Info Science
+   	private String id; // short name for courses SOFE, ELEE, STAT, etc
+   	private Vector<Course> courseList; // all courses offered by the department
+   	private Vector<Student> registerList; // all students taking courses in the department.
 
    
-   public Department(String name, String id) {
-	      // also initialize the vectors
-	   
-	   }
+   	public Department(String name, String id) {
+		this.name = name;
+		this.id = id;
+		this.courseList = new Vector<Course>;
+		this.registerList = new Vector<Student>;
+	}
 
-   public String getName() {
-	   
-   }
+   	public String getName() {
+	   	return this.name;
+   	}
 
-   public String getId() {
-	  
-   }
+   	public String getId() {
+		return this.id;
+   	}
    
  	
-   public String toString() {
-      // returns a string representation of department name, number 
-      // of courses offered and number of students registered in the
-      // department. Use the format:
-      // ECSE: 53 courses, 460 students     
-   }
+   	public String toString() {
+      		return (id + ": " + courseList.length() + " courses, " + registerList.length() + " students");
+		// returns a string representation of department name, number 
+      		// of courses offered and number of students registered in the
+      		// department. Use the format:
+      		// ECSE: 53 courses, 460 students     
+   	}
 }
