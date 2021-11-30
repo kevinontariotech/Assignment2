232b186e45c89dd735aaffcdc92654919be8e633
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2021-11-30 10:21:41 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,22 +1,23 @@
 public class Person {
-      private String   name;
-   
-   
-      public Person(String initialName) {
-         this.name = initialName;
-      }
-   
-      public Person() {}
-   
-      public void setName( String fullName) {
-         this.name = fullName;
-      }
+    private String name;
 
-      public String getName() {
-         return this.name;
-      }
- 
-      public String toString() {
-         return this.name;
-      }
+
+    public Person(String initialName) {
+        this.name = initialName;
+    }
+
+    public Person() {
+    }
+
+    public void setName(String fullName) {
+        this.name = fullName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
